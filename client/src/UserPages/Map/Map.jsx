4c1fb1cd0097cbfd25344fa4be89a859990090de
Jaeyun Jung <rangyun36@gmail.com>
2022-06-23 04:03:06 +0900

    Fix: 처음 위치로 돌아가는 버그 수정

--- client/src/UserPages/Map/Map.jsx
+++ client/src/UserPages/Map/Map.jsx
@@ -15,15 +15,21 @@ function Map() {
 
   //고객이 입력한 검색어
   const [inputLocation, setInputLocation] = useState(false);
+  const [isFirst, setIsFirst] = useState(false);
 
 
 
-  var watcherID = navigator.geolocation.watchPosition(function (position) {
-    setLatitude(position.coords.latitude);
-    setLongitude(position.coords.longitude);
-  });
+    var watcherID = navigator.geolocation.watchPosition(function (position) {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+      setIsFirst(true);
+    });
 
-  // navigator.geolocation.clearWatch(watcherID); // 위치 갱신 그만 두기
+
+    navigator.geolocation.clearWatch(watcherID); // 위치 갱신 그만 두기
+  
+
+  
 
   useEffect(() => {
 
@@ -43,7 +49,7 @@ function Map() {
     var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
     // HTML5의 geolocation으로 사용할 수 있는지 확인합니다
-    if (navigator.geolocation) {
+    if (navigator.geolocation && !isFirst) {
       // GeoLocation을 이용해서 접속 위치를 얻어옵니다
       navigator.geolocation.getCurrentPosition(function (position) {
         var lat = position.coords.latitude, // 위도
@@ -54,8 +60,24 @@ function Map() {
 
         // 마커와 인포윈도우를 표시합니다
         displayMarker(locPosition, message);
+
+        setIsFirst(true);
       });
-    } else {
+    } 
+    else if(navigator.geolocation && isFirst){
+       // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+       navigator.geolocation.getCurrentPosition(function (position) {
+        var lat = latitude, // 위도
+          lon = longitude; // 경도
+
+        var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+          message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
+
+        // 마커와 인포윈도우를 표시합니다
+        displayMarker(locPosition, message);
+      });
+    }
+    else {
       // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
 
       var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),
