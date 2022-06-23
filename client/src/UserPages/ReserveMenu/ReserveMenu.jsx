409da7a68ef424ce74ba6972c8ab5aae4960e327
sik9252 <sik9252@naver.com>
2022-06-24 00:04:52 +0900

    메뉴 선택부분 디자인 수정 & 좌석 예약 select 추가

--- client/src/UserPages/ReserveMenu/ReserveMenu.jsx
+++ client/src/UserPages/ReserveMenu/ReserveMenu.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 import "./ReserveMenu.css";
 
@@ -11,7 +10,7 @@ function ReserveMenu() {
   const [ownerPickItem, setOwnerPickItem] = useState([]);
   const [isPickItem, setIsPickItem] = useState(false);
   const [checkItemNum, setCheckItemNum] = useState([]);
-  const [checkButtonClicked, setCheckButtonClicked] = useState(false)
+  const [checkButtonClicked, setCheckButtonClicked] = useState(false);
 
   const [menuData, setMenuData] = useState([
     {
@@ -20,7 +19,7 @@ function ReserveMenu() {
       name: "아메리카노",
       price: 4000,
       ownerPick: false,
-      menuImg: "/Assets/reserve/coffee"
+      menuImg: "/Assets/test.png",
     },
     {
       id: 2,
@@ -28,7 +27,7 @@ function ReserveMenu() {
       name: "카페라떼",
       price: 4500,
       ownerPick: false,
-      menuImg: "/Assets/reserve/coffee"
+      menuImg: "/Assets/test.png",
     },
     {
       id: 3,
@@ -36,7 +35,7 @@ function ReserveMenu() {
       name: "아인슈페너",
       price: 5000,
       ownerPick: true,
-      menuImg: "/Assets/reserve/coffee"
+      menuImg: "/Assets/test.png",
     },
     {
       id: 4,
@@ -44,18 +43,17 @@ function ReserveMenu() {
       name: "카라멜마끼아또",
       price: 4500,
       ownerPick: false,
-      menuImg:  "/Assets/reserve/coffee"
+      menuImg: "/Assets/test.png",
     },
   ]);
 
-  useEffect(()=>{
+  useEffect(() => {
     //API넣기
 
-    checkOwnerPick()
+    checkOwnerPick();
+  }, []);
 
-  },[])
-
-  useEffect(()=>{
+  useEffect(() => {
     ownerPickItem.filter((item, i) => {
       return (
         ownerPickItem.findIndex((item2, j) => {
@@ -63,34 +61,31 @@ function ReserveMenu() {
         }) === i
       );
     });
-  },[ownerPickItem])
-
+  }, [ownerPickItem]);
 
   //메뉴 선택 변경시 리랜더링
-  useEffect(() =>{
+  useEffect(() => {
     GetThisItemNum();
     setCheckButtonClicked(false);
-  },[checkButtonClicked])
-
+  }, [checkButtonClicked]);
 
   //사장님 픽이 있는 지 검증& 선택 수량 체크를 위한 로직
   const checkOwnerPick = () => {
     menuData.map((thisData) => {
-      if(!thisData.ownerPick === false){
+      if (!thisData.ownerPick === false) {
         setIsPickItem(true);
-        setOwnerPickItem(data => [...data, thisData]);
+        setOwnerPickItem((data) => [...data, thisData]);
 
         //수량체크용
-        var tmp = {id: thisData.id, num: 0}
-        setCheckItemNum(data => [...data, tmp])
+        var tmp = { id: thisData.id, num: 0 };
+        setCheckItemNum((data) => [...data, tmp]);
       }
-    })
-  }
-
+    });
+  };
 
   //수량 체크
   const plusReserveList = (e) => {
-    setCheckButtonClicked(true)
+    setCheckButtonClicked(true);
 
     var targetId = e.target.id;
     var targetPrice = 0;
@@ -99,11 +94,11 @@ function ReserveMenu() {
     var different = true;
 
     //선택한 상품의 가격정보 받아오기
-    menuData.map((thisData)=>{
-      if(thisData.id == targetId){
+    menuData.map((thisData) => {
+      if (thisData.id == targetId) {
         targetPrice = thisData.price;
       }
-    })
+    });
 
     // //상태관리를 위한 숫자 기록
     // checkItemNum.map((thisData) => {
@@ -112,38 +107,39 @@ function ReserveMenu() {
     //   }
     // })
 
-
     //담은 상품이 없는 경우(새로 생성)
-    if(!allItemList || allItemList == undefined){
-      allItemList = [{itemId: targetId,itemNum: 1, itemPrice: targetPrice}]
-        //로컬에 저장
+    if (!allItemList || allItemList == undefined) {
+      allItemList = [{ itemId: targetId, itemNum: 1, itemPrice: targetPrice }];
+      //로컬에 저장
       localStorage.setItem("itemList", JSON.stringify(allItemList));
       return;
     }
     //기존에 담은 상품이 있는 경우
-    else{
-      allItemList.map((thisData) =>{
+    else {
+      allItemList.map((thisData) => {
         //동일한 상품이 있는 경우 수량 갱신
-        if(thisData.itemId == targetId){
+        if (thisData.itemId == targetId) {
           thisData.itemNum += 1;
           different = false;
         }
       });
 
       //다른 상품을 추가하는 경우
-      if(different){
-        allItemList.push({itemId: targetId,itemNum: 1, itemPrice: targetPrice})
+      if (different) {
+        allItemList.push({
+          itemId: targetId,
+          itemNum: 1,
+          itemPrice: targetPrice,
+        });
       }
-      
     }
 
     //로컬에 저장
-    localStorage.setItem("itemList",JSON.stringify(allItemList));
-    }    
-  
+    localStorage.setItem("itemList", JSON.stringify(allItemList));
+  };
 
   const minusReserveList = (e) => {
-    setCheckButtonClicked(true)
+    setCheckButtonClicked(true);
     var targetId = e.target.id;
     var targetPrice = 0;
     var itemNum = 0;
@@ -151,23 +147,22 @@ function ReserveMenu() {
     var allItemList = JSON.parse(localStorage.getItem("itemList"));
 
     //선택한 상품의 가격정보 받아오기
-    menuData.map((thisData)=>{
-      if(thisData.id == targetId){
+    menuData.map((thisData) => {
+      if (thisData.id == targetId) {
         targetPrice = thisData.price;
       }
-    })
-
+    });
 
     //담은 상품이 없는 경우(새로 생성)
-    if(!allItemList){
+    if (!allItemList) {
       return;
     }
     //기존에 담은 상품이 있는 경우
-    else{
-      allItemList.map((thisData) =>{
+    else {
+      allItemList.map((thisData) => {
         //동일한 상품이 있는 경우 수량 갱신
-        if(thisData.itemId == targetId){
-          if(thisData.itemNum == 0){
+        if (thisData.itemId == targetId) {
+          if (thisData.itemNum == 0) {
             return;
           }
           thisData.itemNum -= 1;
@@ -177,60 +172,68 @@ function ReserveMenu() {
 
     //로컬에 저장
     localStorage.setItem("itemList", JSON.stringify(allItemList));
-  }    
-
+  };
 
   //현재 상품의 개수 조회
-  const GetThisItemNum = (target) =>{
+  const GetThisItemNum = (target) => {
     var result = 0;
-    var target = JSON.stringify(target)
+    var target = JSON.stringify(target);
 
     var allItemList = JSON.parse(localStorage.getItem("itemList"));
-    
+
     //담은 상품이 없는 경우(새로 생성)
-    if(allItemList == undefined){
+    if (allItemList == undefined) {
       return result;
-    }
-    else{
-      allItemList.map((thisData) =>{
+    } else {
+      allItemList.map((thisData) => {
         //동일한 상품이 있는 경우 수량 갱신
-        if(thisData.itemId == target){
-          result = thisData.itemNum   
+        if (thisData.itemId == target) {
+          result = thisData.itemNum;
         }
-      })
-  }
+      });
+    }
     return result;
-  }
+  };
 
   return (
     <div className="reserve-seat-container">
-        <div>
-          {menuData.map((thisMenu) => {
-            return (
-              <div className="menu-card">
-                <div className="menu-img">
-                  <img src={thisMenu.menuImg} alt={thisMenu.name} />
-                </div>
-                <div className="menu-info">
-                  <div className="menu-name">{thisMenu.name}</div>
-                  <div className="menu-name">{thisMenu.price}</div>
-                  <div className="menu-name">
-                    <img id={thisMenu.id} className="buttonStyle" src="/Assets/reserve/minus.png" alt="minus" onClick={minusReserveList}/>
-                      {/* <GetThisItemNum target={thisMenu.id}/> */}
-                      {
-                        GetThisItemNum(thisMenu.id)
-                      }
-                    <img id={thisMenu.id} className="buttonStyle"  src="/Assets/reserve/plus.png" alt="plus" onClick={plusReserveList}/>
-                  </div>
-                  {/* <input */}  
-                </div>
+      <div>
+        {menuData.map((thisMenu) => {
+          return (
+            <div className="menu-card">
+              <div className="menu-img">
+                <img src={thisMenu.menuImg} alt={thisMenu.name} />
               </div>
-            );
-          })}
-        </div>
-        <div className="order-frame">
-          <div className="order-btn">주문하기</div>
-        </div>
+              <div className="menu-info">
+                <div className="menu-name">{thisMenu.name}</div>
+                <div className="menu-price">{thisMenu.price}</div>
+                <div className="menu-count">
+                  <img
+                    id={thisMenu.id}
+                    className="buttonStyle"
+                    src="/Assets/reserve/minus.png"
+                    alt="minus"
+                    onClick={minusReserveList}
+                  />
+                  {/* <GetThisItemNum target={thisMenu.id}/> */}
+                  {GetThisItemNum(thisMenu.id)}
+                  <img
+                    id={thisMenu.id}
+                    className="buttonStyle"
+                    src="/Assets/reserve/plus.png"
+                    alt="plus"
+                    onClick={plusReserveList}
+                  />
+                </div>
+                {/* <input */}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="order-frame">
+        <div className="order-btn">주문하기</div>
+      </div>
     </div>
   );
 }
--- client/src/UserPages/ReserveSeat/ReserveSeat.jsx
+++ client/src/UserPages/ReserveSeat/ReserveSeat.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-
 import "./ReserveSeat.css";
 
 function ReserveSeat() {
@@ -25,28 +24,38 @@ function ReserveSeat() {
       status: "혼잡",
     },
     {
-      id: 4,
+      id: 5,
+      seatName: "룸",
+      status: "혼잡",
+    },
+    {
+      id: 6,
       seatName: "룸",
       status: "혼잡",
     },
   ]);
+
   return (
-    <div className="reserve-seat-container">
-      {seatData.map((seat) => {
-        return (
-          <div className="seat-card">
-            <div className="seat-img">
-              <img src="/Assets/seattest.png" alt="" />
+    <>
+      <div className="reserve-seat-container">
+        {seatData.map((seat) => {
+          return (
+            <div className="seat-card">
+              <div className="seat-img">
+                <img src="/Assets/seattest.png" alt="" />
+              </div>
+              <div className="seat-info">
+                <div className="seat-name">{seat.seatName}</div>
+                <div className="seat-status">{seat.status}</div>
+                <div className="seat-text">
+                  인원 수 만큼 음료를 구매해주세요.
+                </div>
+              </div>
             </div>
-            <div className="seat-info">
-              <div className="seat-name">{seat.seatName}</div>
-              <div className="seat-status">{seat.status}</div>
-              <div className="seat-text">인원 수 만큼 음료를 구매해주세요.</div>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
