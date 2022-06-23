422e7e34c255519991d6608cb85464e1d9a0e338
sik9252 <sik9252@naver.com>
2022-06-23 19:17:35 +0900

    예약 페이지의 좌석예약/메뉴선택 레이아웃 구현중

--- client/src/UserPages/ReserveSeat/ReserveSeat.jsx
+++ client/src/UserPages/ReserveSeat/ReserveSeat.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import "./ReserveSeat.css";
 
 function ReserveSeat() {
-  const [seatData, setDeatData] = useState([
+  const [seatData, setSeatData] = useState([
     {
       id: 1,
       seatName: "1인석",
@@ -24,6 +24,11 @@ function ReserveSeat() {
       seatName: "룸",
       status: "혼잡",
     },
+    {
+      id: 4,
+      seatName: "룸",
+      status: "혼잡",
+    },
   ]);
   return (
     <div className="reserve-seat-container">
@@ -35,8 +40,8 @@ function ReserveSeat() {
             </div>
             <div className="seat-info">
               <div className="seat-name">{seat.seatName}</div>
-              <div className="seat-name">{seat.status}</div>
-              <div className="seat-name">인원 수 만큼 음료를 구매해주세요.</div>
+              <div className="seat-status">{seat.status}</div>
+              <div className="seat-text">인원 수 만큼 음료를 구매해주세요.</div>
             </div>
           </div>
         );
