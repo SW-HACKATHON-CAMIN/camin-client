5a0cad0ae6571607f2f721c2d7d2a7ea41024990
sik9252 <sik9252@naver.com>
2022-06-23 15:58:44 +0900

    좌석 예약 레이아웃 작업중

--- client/src/UserPages/ReserveSeat/ReserveSeat.jsx
+++ client/src/UserPages/ReserveSeat/ReserveSeat.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import "./ReserveSeat.css";
 
 function ReserveSeat() {
+  const [seatData, setDeatData] = useState([
+    {
+      id: 1,
+      seatName: "1인석",
+      status: "혼잡",
+    },
+    {
+      id: 2,
+      seatName: "다인석",
+      status: "혼잡",
+    },
+    {
+      id: 3,
+      seatName: "1인석",
+      status: "혼잡",
+    },
+    {
+      id: 4,
+      seatName: "룸",
+      status: "혼잡",
+    },
+  ]);
   return (
-    <div>ReserveSeat</div>
-  )
+    <div className="reserve-seat-container">
+      {seatData.map((seat) => {
+        return (
+          <div className="seat-card">
+            <div className="seat-img">
+              <img src="/Assets/seattest.png" alt="" />
+            </div>
+            <div className="seat-info">
+              <div className="seat-name">{seat.seatName}</div>
+              <div className="seat-name">{seat.status}</div>
+              <div className="seat-name">인원 수 만큼 음료를 구매해주세요.</div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default ReserveSeat
+export default ReserveSeat;
